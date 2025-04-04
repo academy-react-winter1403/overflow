75fe437c1e2c34d426dd986b7a33422a782646ea
amirzex <154665688+amirzex@users.noreply.github.com>
2025-04-04 21:12:35 +0330

    api for courses page

---
+++ src/core/services/api/GetCourses/Getapi.js
@@ -0,0 +1,31 @@
+import http from '../../interceptor/index.js';
+
+const GetCourses = async () => {
+  try {
+
+    const response = await http.get('/Home/GetCoursesTop?Count=5');
+    return response.data;
+    
+  } catch (error) {
+    console.log("Error fetching courses:", error.message);
+    return false;
+
+  }
+};
+
+
+const Getteachers = async (token) => {
+  try {
+    const response = await http.get('/Course/TeacherCourseList?PageNumber=1&RowsOfPage=10&SortingCol=DESC&SortType=Expire&Query', {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log("Error fetching teachers:", error.message);
+    return false;
+  }
+};
+
+export { GetCourses,Getteachers };
