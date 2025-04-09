5b88dbcdf490e267b19486c5755af4b65f72a037
n4vdz <navidhosseini.q@gmail.com>
2025-04-09 11:50:19 +0330

    style landing

--- src/components/Landing/Hero.jsx
+++ src/components/Landing/Hero.jsx
@@ -7,10 +7,10 @@ function Hero() {
   return (
     <section className="flex flex-row-reverse items-center justify-between px-10 py-16 dark:bg-gray-900">
       <div className="w-full flex-shrink-2  md:w-1/2 text-right space-y-6">
-        <h1 className=" text-3xl md:text-4xl font-bold text-deep-blue dark:text-white">
+        <h1 className=" text-[59px]  font-extrabold font-peyda  text-deep-blue dark:text-white">
           دنبال چی می‌گردی؟
         </h1>
-        <p className="text-gray-500 dark:text-gray-300">
+        <p className="text-gray-500  dark:text-gray-300">
           دیگه وقتشه یه نقشه‌ای به خودت بدی...
         </p>
 
@@ -22,9 +22,7 @@ function Hero() {
             className="w-full md:w-[300px] h-[50px] rounded-full border border-none px-4 text-right focus:outline-none"
           />
           <button className="w-[55px] h-[53px] flex-shrink-0 bg-deep-blue text-white rounded-2xl">
-            <img src={search}
-            className="w-[38px] h-[38px] mx-auto my-auto"
-            />
+            <img src={search} className="w-[38px] h-[38px] mx-auto my-auto" />
           </button>
         </div>
 
