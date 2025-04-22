03049bda29c03c05536ae5e073740475e75976e8
n4vdz <navidhosseini.q@gmail.com>
2025-04-22 21:57:57 +0330

    reply

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import user from "../../assets/Header/user.png";
+import ReplyComment from "./ReplyComment.jsx";
 import {
   GetComment,
   Likecommnet,
@@ -12,19 +13,12 @@ import disLike from "../../assets/Coursesimage/dislike.png";
 import { getItem } from "../../core/services/common/storage.services.js";
 
 const Comment = ({ commentData, id, commentsCount }) => {
-  const [focusedCommentId, setFocusedCommentId] = useState(null); // Track focused comment by ID
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   console.log("commentData", commentData);
+  const handelLike=
 
-  const handleFocus = (commentId) => {
-    setFocusedCommentId(commentId); // Set the focused comment ID
-  };
-
-  const handleBlur = () => {
-    setFocusedCommentId(null); // Reset focused comment when blur
-  };
 
   return (
     <div className="w-full">
@@ -62,7 +56,8 @@ const Comment = ({ commentData, id, commentsCount }) => {
 
               <div className="relative mx-3 flex items-center justify-between">
                 <div className="flex gap-3">
-                  <button className="bg-deep-blue/25 hover:bg-deep-blue/55 flex cursor-pointer rounded-xl p-1 transition-all hover:scale-110">
+                  <button 
+                  className="bg-deep-blue/25 hover:bg-deep-blue/55 flex cursor-pointer rounded-xl p-1 transition-all hover:scale-110">
                     <img src={Like} alt="Like" className="inline-block" />
                     <p className="p-1">
                       {comment.likeCount > 0 ? comment.likeCount : ""}
@@ -71,33 +66,15 @@ const Comment = ({ commentData, id, commentsCount }) => {
                   <button className="bg-deep-blue/25 hover:bg-deep-blue/55 flex cursor-pointer rounded-xl p-1 transition-all hover:scale-110">
                     <img src={disLike} alt="Dislike" />
                     <p className="p-1">
-                      {comment.disslikeCount > 0 ? comment.disslikeCount : ""}
+                      {comment.dissLikeCount > 0 ? comment.dissLikeCount : ""}
                     </p>
                   </button>
                 </div>
 
-                <div
-                  className={`max-w-2/3 min-w-1/2 rounded-xl bg-gray-100 p-2 ${
-                    focusedCommentId === comment.id
-                      ? "h-40 w-full"
-                      : "h-10 w-1/2"
-                  } transition-all duration-300`}
-                >
-                  <div className="flex w-full items-center justify-between">
-                    {focusedCommentId == comment.id && (
-                      <button className="cursor-pointer rounded-xl px-2 pb-1 hover:text-black">
-                        ثبت
-                      </button>
-                    )}
-                    <textarea
-                      style={{ outline: "none", resize:"none" }} // Add this inline style to remove the focus outline
-                      className={`w-full pl-2 ${focusedCommentId === comment.id ? "h-40 w-full" : "h-10 w-1/2"} transition-all duration-300`}
-                      placeholder="Write your reply here..."
-                      onFocus={() => handleFocus(comment.id)} // On focus, set focused comment
-                      onBlur={handleBlur} // On blur, reset focus
-                    ></textarea>
-                  </div>
-                </div>
+                <>
+                <ReplyComment commentId={comment.id} />
+                  
+                </>
               </div>
             </div>
           ))
