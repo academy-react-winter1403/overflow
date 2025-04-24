54aa1573fc4cfea39da5290b80829ac575b2fe6e
n4vdz <navidhosseini.q@gmail.com>
2025-04-24 16:15:52 +0330

    Update News.jsx

--- src/components/Landing/News.jsx
+++ src/components/Landing/News.jsx
@@ -9,7 +9,7 @@ import newsPath from "../../assets/landing/path2.png";
 function News() {
   const URL =
     "/News?PageNumber=1&RowsOfPage=4&SortingCol=InsertDate&SortType=DESC";
-
+  const [newsSlider, setNewsSlider] = useState(0);
   const [newsData, setNewsData] = useState([""]);
   useEffect(() => {
     getNews();
@@ -19,68 +19,77 @@ function News() {
     setNewsData(response);
     // console.log(response);
   };
-
+  const nextNews = () => {
+    setNewsSlider((prev) => prev + 1);
+  };
+  const prevNews = () => {
+    setNewsSlider((prev) => Math.max(prev - 1, 0)); // Prevent going below 0
+  };
   return (
-    <div className=" relative flex flex-col container mx-auto py-10">
-      <div className=" z-0">
+    <div className="relative container mx-auto flex flex-col py-10">
+      <div className="z-0">
         <img
           src={bubbleBack}
           alt=""
-          className="absolute w-[1511px] h-[1511px] opacity-98 -top-168 -left-190 "
+          className="absolute -top-168 -left-190 h-[1511px] w-[1511px] opacity-98"
         />
         <img
           src={newsPath}
           alt=""
-          className="absolute w-[809px] h-[774px] opacity-98 -top-10 -left-65 "
+          className="absolute -top-10 -left-65 h-[774px] w-[809px] opacity-98"
         />
       </div>
-      <h2 className="relative mb-14 text-5xl z-10 font-peyda font-black text-deep-blue">
+      <h2 className="font-peyda text-deep-blue relative z-10 mb-14 text-5xl font-black">
         آخرین اخبار
       </h2>
-      <section className=" justify-center flex overflow-hidden relative z-10">
-        <div className=" w-264 h-119 relative items-center flex flex-col space-y-6">
-          <div className="absolute   bg-white opacity-80 w-264 h-99 rounded-sm  shadow-[0px_5px_27.5px_rgba(0,0,0,0.16)] "></div>
+      <section className="relative z-10 flex justify-center overflow-hidde">
+        <div className=" flex h-119 w-264 flex-col items-center space-y-6">
+          <div className="absolute h-99 w-264 rounded-sm bg-white opacity-80 shadow-[0px_5px_27.5px_rgba(0,0,0,0.16)]"></div>
 
           {newsData &&
             newsData.map((newsItem, index) => (
-              <div key={index} className="mt-11 mr-16 z-10 ">
-                <div className="bg-white w-264 h-99 rounded-sm  shadow-[0px_5px_27.5px_rgba(0,0,0,0.16)] py-8 px-11 flex flex-col md:flex-row-reverse gap-6 text-right">
+              <div key={index} className={`-translate-y-${newsSlider*12}/10  z-10 mt-11 mr-16`}>
+                <div className="flex h-99 w-264 flex-col gap-6 rounded-sm bg-white px-11 py-8 text-right shadow-[0px_5px_27.5px_rgba(0,0,0,0.16)] md:flex-row-reverse">
                   {/* Image */}
-                  <div className="md:w-1/3 w-full flex justify-center">
+                  <div className="flex w-full justify-center md:w-1/3">
                     <SmartImage
                       src={newsItem.currentImageAddressTumb}
                       fallback={newsDef}
                       alt="News"
-                      className="w-[328px] h-[328px] object-cover rounded-md"
+                      className="h-[328px] w-[328px] rounded-md object-cover"
                     />
                   </div>
 
                   {/* Text content */}
-                  <div className="md:w-2/3 w-full space-y-4 ">
-                    <h3 className="mt-16 text-5xl font-kalameh font-bold text-gray-700">
+                  <div className="w-full space-y-4 md:w-2/3">
+                    <h3 className="font-kalameh mt-16 text-5xl font-bold text-gray-700">
                       {newsItem.title}
                     </h3>
-                    <p className="text-gray-600 text-2xl font-vazir leading-relaxed">
+                    <p className="font-vazir text-2xl leading-relaxed text-gray-600">
                       {newsItem.miniDescribe}
                     </p>
                   </div>
                 </div>
-
               </div>
             ))}
         </div>
-        <div className="flex items-center  mb-6">
-          <div className="flex flex-col items-center ml-8 space-x-2">
-            <img src={up} className="w-4 hover:cursor-pointer" />
-            <div className="font-vazir my-2 text-2xl font-bold text-gray-700 ">
-              1 / 4
+        <div className="mb-6 flex items-center">
+          <div className="ml-8 flex flex-col items-center space-x-2">
+            <button onClick={nextNews}>
+              <img src={up} className="w-4 hover:cursor-pointer" />
+            </button>
+
+            <div className="font-vazir my-2 text-2xl font-bold text-gray-700">
+              {newsSlider + 1}/ 4
             </div>
-            <img src={up} className="rotate-180 w-4 hover:cursor-pointer " />
+            <button onClick={prevNews}>
+              <img src={up} className="w-4 rotate-180 hover:cursor-pointer" />
+            </button>
           </div>
         </div>
       </section>
-      <div className=" text-left w-full">
-        <button className="text-gray-700 font-bold font-vazir text-2xl hover:cursor-pointer self-start">
+      <div className="w-full text-left">
+        <button className="font-vazir self-start text-2xl font-bold text-gray-700 hover:cursor-pointer">
           مشاهده همه اخبار
         </button>
       </div>
