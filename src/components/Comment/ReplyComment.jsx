b5d4aed402f69a6eb47a1826f98cda2b438ba021
n4vdz <navidhosseini.q@gmail.com>
2025-04-24 15:37:06 +0330

    news details, responsive

--- src/components/Comment/ReplyComment.jsx
+++ src/components/Comment/ReplyComment.jsx
@@ -12,7 +12,7 @@ function ReplyComment({commentId}) {
 
   return (
       <div
-        className={`max-w-2/3 min-w-1/2 rounded-xl bg-gray-100 p-2 ${
+        className={`max-w-2/3 min-w-1/2 rounded-xl bg-gray-100 dark:text-amber-50 dark:bg-deep-blue/35 p-2 ${
           focusedCommentId === commentId ? "h-40 w-full" : "h-10 w-1/2"
         } transition-all duration-300`}
       >
