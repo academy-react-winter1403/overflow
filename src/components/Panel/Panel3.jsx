733ad48d653e5a9308180749a6c8049b88b0c411
amirzex <154665688+amirzex@users.noreply.github.com>
2025-06-10 20:39:28 +0330

    basket responsive /.....

--- src/components/Panel/Panel3.jsx
+++ src/components/Panel/Panel3.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-
-import Card from "../Common/Card";
-
 import { Getmycourse } from "../../core/services/api/userpanelapi/panelapis";
+import MycourseCard from "../Common/MycourseCard";
 
 const Panel3 = () => {
   const navigate = useNavigate();
@@ -35,28 +33,46 @@ const Panel3 = () => {
   };
 
   const filtermycours = newCoursesData.filter(
-    (mycourse) => mycourse?.courseTitle?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    (mycourse) =>
+      mycourse?.courseTitle
+        ?.toLowerCase()
+        .includes(searchQuery.toLowerCase()) ||
       mycourse?.describe?.toLowerCase().includes(searchQuery.toLowerCase()) ||
       mycourse?.fullName?.toLowerCase().includes(searchQuery.toLowerCase()),
   );
 
   return (
     <div className="font-kalameh flex w-10/10 flex-row-reverse flex-wrap ">
-
       <div className="flex w-10/10 flex-row-reverse flex-wrap">
-            <div>
-        <input
-          type="text"
-          placeholder="جستجو دوره..."
-          className="w-90 rounded-lg mt-5 mr-5 border border-gray-400 p-2 text-right max-sm:w-50"
-          value={searchQuery}
-          onChange={(mycourse) => setSearchQuery(mycourse.target.value)}
-        />
-      </div>
-        <div className="mt-10 flex h-155 w-10/10 flex-row-reverse flex-wrap justify-center gap-8 max-lg:h-150 max-lg:overflow-auto max-sm:h-150 max-sm:overflow-auto max-sm:gap-0 ">
+        <div>
+          <input
+            type="text"
+            placeholder="جستجو دوره..."
+            className="mt-5 mr-5 w-90 rounded-lg border border-gray-400 p-2 text-right max-sm:w-50"
+            value={searchQuery}
+            onChange={(mycourse) => setSearchQuery(mycourse.target.value)}
+          />
+        </div>
+
+        <div className="mt-10 flex h-155 w-10/10 flex-col items-end gap-8 overflow-auto max-lg:h-150 max-lg:overflow-auto max-sm:h-150 max-sm:gap-0 max-sm:overflow-auto">
+          <div className="border-deep-blue flex flex-row-reverse justify-center border-b-4 pr-10 max-md:justify-start max-md:gap-8 max-md:text-xl w-10/10">
+            <p className="w-4/10 text-center text-xl font-bold  transition-all duration-300 max-lg:mr-[-100px] max-lg:w-5/10 max-md:mr-[-1px]">
+              نام دوره
+            </p>
+            <p className="max-xl:hidden w-3/10 text-center text-xl font-bold pl-15 transition-all duration-300 max-xl:mr-[-65px] max-lg:w-5/10 max-lg:text-center max-md:hidden">
+              مدرس دوره
+            </p>
+            <p className="w-3/10   text-right pr-15 text-xl font-bold transition-all duration-300 max-lg:hidden ">
+             تاریخ شروع
+
+            </p>
+            <p className="w-3/10 text-xl font-bold pl-30 text-center max-xl:pl-0 transition-all duration-300 ">
+              قیمت
+            </p>
+          </div>
           {filtermycours.length > 0 ? (
             filtermycours.map((course, index) => (
-              <Card
+              <MycourseCard
                 item={course}
                 index={index}
                 handleNavigation={handleNavigation}
